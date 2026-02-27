f9da4ed91df1133aa2dd3155c7e47b27edab20cf
GayathriVasanthan <gayathrivasanthan0609@gmail.com>
2026-02-27 10:16:26 +0530

    UC11 : Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,36 @@
-public class PalindromeCheckerApp {
+import java.util.Stack;
 
+    public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
 
-            String original = "string";
-            String reversed = "";
+            PalindromeChecker checker = new PalindromeChecker();
+            String word = "noon";
 
-            for (int i = original.length() - 1; i >= 0; i--) {
-                reversed = reversed + original.charAt(i);
-            }
-
-            if (original.equals(reversed)) {
-                System.out.println(original + " is a Palindrome.");
+            if (checker.checkPalindrome(word)) {
+                System.out.println(word + " is a Palindrome.");
             } else {
-                System.out.println(original + " is NOT a Palindrome.");
+                System.out.println(word + " is NOT a Palindrome.");
             }
         }
+    }
+
+    class PalindromeChecker {
+
+        public boolean checkPalindrome(String word) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < word.length(); i++) {
+                stack.push(word.charAt(i));
+            }
+
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 
 
