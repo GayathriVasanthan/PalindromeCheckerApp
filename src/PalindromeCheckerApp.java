0fed60131f05819c8151564c234d59968ef47f91
GayathriVasanthan <gayathrivasanthan0609@gmail.com>
2026-02-27 10:18:52 +0530

    UC12 : Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,61 @@
-public class PalindromeCheckerApp {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 
+    public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
 
-            String original = "string";
-            String reversed = "";
+            String word = "rotor";
 
-            for (int i = original.length() - 1; i >= 0; i--) {
-                reversed = reversed + original.charAt(i);
-            }
+            PalindromeStrategy strategy;
 
-            if (original.equals(reversed)) {
-                System.out.println(original + " is a Palindrome.");
-            } else {
-                System.out.println(original + " is NOT a Palindrome.");
-            }
+            strategy = new StackStrategy();
+            System.out.println("Using StackStrategy: " + word + " is " +
+                    (strategy.isPalindrome(word) ? "" : "NOT ") + "a Palindrome.");
+
+            strategy = new DequeStrategy();
+            System.out.println("Using DequeStrategy: " + word + " is " +
+                    (strategy.isPalindrome(word) ? "" : "NOT ") + "a Palindrome.");
         }
     }
+
+    interface PalindromeStrategy {
+        boolean isPalindrome(String word);
+    }
+
+    class StackStrategy implements PalindromeStrategy {
+
+        public boolean isPalindrome(String word) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < word.length(); i++) {
+                stack.push(word.charAt(i));
+            }
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    class DequeStrategy implements PalindromeStrategy {
+
+        public boolean isPalindrome(String word) {
+            Deque<Character> deque = new LinkedList<>();
+            for (int i = 0; i < word.length(); i++) {
+                deque.addLast(word.charAt(i));
+            }
+            while (deque.size() > 1) {
+                if (!deque.removeFirst().equals(deque.removeLast())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 
 
 
