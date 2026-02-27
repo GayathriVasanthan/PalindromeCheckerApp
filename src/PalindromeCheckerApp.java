922ada112e8600add47b3c3f126c93c0d8afd2de
GayathriVasanthan <gayathrivasanthan0609@gmail.com>
2026-02-27 10:21:36 +0530

    UC13 : Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,71 @@
-public class PalindromeCheckerApp {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 
+    public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
 
-            String original = "string";
-            String reversed = "";
+            String word = "wow";
 
-            for (int i = original.length() - 1; i >= 0; i--) {
-                reversed = reversed + original.charAt(i);
-            }
+            long start, end;
 
-            if (original.equals(reversed)) {
-                System.out.println(original + " is a Palindrome.");
-            } else {
-                System.out.println(original + " is NOT a Palindrome.");
-            }
+            start = System.nanoTime();
+            boolean resultStack = checkWithStack(word);
+            end = System.nanoTime();
+            System.out.println("Stack approach: " + resultStack + " | Time: " + (end - start) + " ns");
+
+            start = System.nanoTime();
+            boolean resultDeque = checkWithDeque(word);
+            end = System.nanoTime();
+            System.out.println("Deque approach: " + resultDeque + " | Time: " + (end - start) + " ns");
+
+            start = System.nanoTime();
+            boolean resultCharArray = checkWithCharArray(word);
+            end = System.nanoTime();
+            System.out.println("Char array approach: " + resultCharArray + " | Time: " + (end - start) + " ns");
         }
+
+        public static boolean checkWithStack(String word) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < word.length(); i++) {
+                stack.push(word.charAt(i));
+            }
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static boolean checkWithDeque(String word) {
+            Deque<Character> deque = new LinkedList<>();
+            for (int i = 0; i < word.length(); i++) {
+                deque.addLast(word.charAt(i));
+            }
+            while (deque.size() > 1) {
+                if (!deque.removeFirst().equals(deque.removeLast())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static boolean checkWithCharArray(String word) {
+            char[] chars = word.toCharArray();
+            int start = 0;
+            int end = chars.length - 1;
+            while (start < end) {
+                if (chars[start] != chars[end]) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+
     }
 
 
